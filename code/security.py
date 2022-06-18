24cfc683f5997788dffe24401d7c9745d46d40cc
sasikumar9211 <sasikumar.balasubramanian92@gmail.com>
2022-06-18 17:14:07 +0530

    Heroku issue fix

--- code/security.py
+++ code/security.py
@@ -1,10 +1,9 @@
 from models.user import UserModel
-from werkzeug.security import safe_str_cmp
 
 def authenticate(username,password):
     user = UserModel.find_by_username(username)
 
-    if user and safe_str_cmp(user.password,password):
+    if user and user.password == password:
         return user
 
 
